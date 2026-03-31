fd7bae212f3df27e58ec12872eedd887d05d756e
Jeremias Verissimo Gomes <131094464+Eujeremias@users.noreply.github.com>
2026-03-31 19:47:47 +0000

    adicionadno rotas do motorista

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,23 +1,43 @@
 import express from "express"
 import { Database } from "../database/DataBase.js"
+
 import { EntregasRepository } from "../repositores/EntregasRepository.js";
 import { EntregasService } from "../services/EntregasServices.js";
 import { EntregasController } from "../controller/EntregasController.js";
+
+import { MotoristasController } from "../controller/MotoristasController.js";
+import { MotoristasRepository } from "../repositores/MotoristasRepository.js";
+import { MotoristasService } from "../services/MotoristasService.js";
 
 const router = express.Router();
 
 // Dependencias
 const dataBase = new Database()
-const repository = new EntregasRepository(dataBase)
-const service = new EntregasService(repository)
-const controller = new EntregasController(service)
 
-// Todas as rotas
-router.post("/entregas",controller.criar)
-router.get("/entregas",controller.listar)
-router.get("/entregas/:id/historico",controller.historico)
-router.get("/entregas/:id",controller.buscarPorId)
-router.patch("/entregas/:id/avancar",controller.avancar)
-router.patch("/entregas/:id/cancelar",controller.cancelar)
+//Entregas
+const entregasRepository = new EntregasRepository(dataBase)
+const entregasService = new EntregasService(entregasRepository)
+const entregasController = new EntregasController(entregasService)
+
+
+//Motorista
+
+const motoristasRepository = new MotoristasController(dataBase)
+const motoristasService = new MotoristasService(motoristasRepository)
+const motoristasController = new MotoristasController(motoristasService)
+
+// Todas - Entregas
+router.post("/entregas",entregasController.criar)
+router.get("/entregas",entregasController.listar)
+router.get("/entregas/:id/historico",entregasController.historico)
+router.get("/entregas/:id",entregasController.buscarPorId)
+router.patch("/entregas/:id/avancar",entregasController.avancar)
+router.patch("/entregas/:id/cancelar",entregasController.cancelar)
+
+// Rotas - Motoristas
+router.post("/motoristas", motoristasController.criar)
+router.get("motoristas", motoristasController.listar)
+router.get("/motoristas/:id", motoristasController.buscarPorId)
+
 
 export default router
